247d53e8ebbdfb9e1328a3a9376cc1f9c9fcb4d3
IvanEnclonar <paulivanenclonar@gmail.com>
2023-04-06 18:23:28 +0800

    Added a lot of new features. Almost finished with the sell menu.

--- ENCLONAR.c
+++ ENCLONAR.c
@@ -13,7 +13,7 @@ int login(struct UserInfo users[], int numUsers)
     {
         if (userID == users[i].userID && strcmp(password, users[i].password) == 0)
         {
-            return 1;
+            return userID;
         }
     }
     printf("Invalid userID or password. Please try again.\n");
@@ -25,6 +25,7 @@ void printMenu()
     printf("\n[1] Register User\n");
     printf("[2] Main Menu\n");
     printf("[0] Exit\n");
+    printf("Enter choice: ");
 }
 
 void printUserMenu()
@@ -32,6 +33,51 @@ void printUserMenu()
     printf("\n[1] Sell Menu\n");
     printf("[2] Buy Menu\n");
     printf("[3] Exit User Menu\n");
+    printf("Enter choice: ");
+}
+
+void SellMenu(int userID, struct Item items[], int *itemCount)
+{
+    int choice = 0;
+    while (choice != 5)
+    {
+        printf("\n[1] Add New Item\n");
+        printf("[2] Edit Stock\n");
+        printf("[3] Show My Products\n");
+        printf("[4] Show low stock products\n");
+        printf("[5] Exit Sell Menu\n");
+        printf("Enter choice: ");
+        scanf(" %d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            if (sellerProductCount(items, *itemCount, userID) >= 20)
+            {
+                printf("You have reached the maximum number of items you can sell.\n");
+                break;
+            }
+            else
+            {
+                items[*itemCount] = addNewItem(userID, uniqueProductID(items, *itemCount));
+                *itemCount = *itemCount + 1;
+                break;
+            }
+        case 2:
+            int productEditID = 0;
+            showMyProducts(items, *itemCount, userID, sellerProductCount(items, *itemCount, userID));
+            printf("Enter the product ID of the item you want to edit: ");
+            scanf(" %d", &productEditID);
+            editStock(items, *itemCount, userID, productEditID);
+            break;
+        case 3:
+            showMyProducts(items, *itemCount, userID, sellerProductCount(items, *itemCount, userID));
+            break;
+        default:
+            printf("Invalid choice!\n");
+            break;
+        }
+    }
 }
 
 int main()
@@ -39,12 +85,11 @@ int main()
     int choice = 1;
     struct UserInfo users[100];
     struct Item items[100];
-    int userCount = 0;
+    int userCount = 0, itemCount = 0;
 
     while (choice != 0)
     {
         printMenu();
-        printf("Enter choice: ");
         scanf(" %d", &choice);
         switch (choice)
         {
@@ -53,17 +98,20 @@ int main()
             userCount++;
             break;
         case 2:
-            if (login(users, userCount))
+            int userID = login(users, userCount);
+            if (userID)
             {
                 int userMenuChoice = 0;
                 printf("Logged in successfully.\n");
                 printUserMenu();
-                printf("Enter choice: ");
                 scanf(" %d", &userMenuChoice);
                 switch (userMenuChoice)
                 {
                 case 1:
-
+                    SellMenu(userID, items, &itemCount);
+                    break;
+                default:
+                    printf("Invalid choice!\n");
                     break;
                 }
             }
--- sellMenu.c
+++ sellMenu.c
@@ -1,11 +1,45 @@
 #include "register.c"
 
-struct Item addNewItem()
+int uniqueProductID(struct Item items[], int numItems)
+{
+    int productID = 0, isUnique = 1;
+
+    do
+    {
+        isUnique = 1;
+        printf("Enter product ID (must be unique): ");
+        scanf("%d", &productID);
+
+        for (int i = 0; i < numItems; i++)
+        {
+            if (items[i].productID == productID)
+            {
+                isUnique = 0;
+                printf("Product ID already exists!\n");
+            }
+        }
+    } while (isUnique == 0);
+
+    return productID;
+}
+
+int sellerProductCount(struct Item items[], int numItems, int sellerID)
+{
+    int count = 0;
+    for (int i = 0; i < numItems; i++)
+    {
+        if (items[i].sellerID == sellerID)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+struct Item addNewItem(int sellerID, int productID)
 {
     struct Item newItem;
 
-    printf("Enter product ID: ");
-    scanf("%d", &newItem.productID);
+    newItem.productID = productID;
     printf("Enter item name: ");
     scanf("%s", newItem.itemName);
     printf("Enter category: ");
@@ -16,8 +50,116 @@ struct Item addNewItem()
     scanf("%d", &newItem.quantityAvailable);
     printf("Enter unit price: ");
     scanf("%f", &newItem.unitPrice);
-    printf("Enter seller ID: ");
-    scanf("%d", &newItem.sellerID);
+    newItem.sellerID = sellerID;
 
+    printf("Item added successfully!\n");
     return newItem;
+}
+
+void sortItems(struct Item items[], int numItems)
+{
+    struct Item temp;
+    for (int i = 0; i < numItems; i++)
+    {
+        for (int j = i + 1; j < numItems; j++)
+        {
+            if (items[i].productID > items[j].productID)
+            {
+                temp = items[i];
+                items[i] = items[j];
+                items[j] = temp;
+            }
+        }
+    }
+}
+
+void showMyProducts(struct Item items[], int numItems, int sellerID, int myProductCount)
+{
+    int count = 0;
+    struct Item myProducts[myProductCount];
+    for (int i = 0; i < numItems; i++)
+    {
+        if (items[i].sellerID == sellerID)
+        {
+            myProducts[count] = items[i];
+            count++;
+        }
+    }
+    sortItems(myProducts, myProductCount);
+
+    printf("\nProduct ID\tItem Name\tCategory\tQuantity Available\tUnit Price\n");
+    for (int i = 0; i < myProductCount; i++)
+    {
+        printf("%10d\t%s\t\t%s\t\t%18d\t%10.2f\n", myProducts[i].productID, myProducts[i].itemName, myProducts[i].category, myProducts[i].quantityAvailable, myProducts[i].unitPrice);
+    }
+}
+
+void printEditMenu()
+{
+    printf("[1] Replenish\n");
+    printf("[2] Change unit price\n");
+    printf("[3] Change item name\n");
+    printf("[4] Change item category\n");
+    printf("[5] Change item description\n");
+    printf("[6] Finish editing\n");
+}
+
+void editStock(struct Item items[], int numItems, int sellerID, int productID)
+{
+    int i, found = 0;
+    for (i = 0; i < numItems; i++)
+    {
+        if (items[i].productID == productID && items[i].sellerID == sellerID)
+        {
+            found = 1;
+            int editChoice = 0;
+            do
+            {
+                printEditMenu();
+                printf("Enter your choice: ");
+                scanf("%d", &editChoice);
+                switch (editChoice)
+                {
+                case 1:
+                    int amt = 0;
+                    printf("Enter quantity to replenish: ");
+                    scanf("%d", &amt);
+                    items[i].quantityAvailable = amt + items[i].quantityAvailable;
+                    printf("Quantity replenished successfully!\n");
+                    break;
+                case 2:
+                    printf("Enter new unit price: ");
+                    scanf("%f", &items[i].unitPrice);
+                    printf("Unit price changed successfully!\n");
+                    break;
+                case 3:
+                    printf("Enter new item name: ");
+                    scanf("%s", items[i].itemName);
+                    printf("Item name changed successfully!\n");
+                    break;
+                case 4:
+                    printf("Enter new item category: ");
+                    scanf("%s", items[i].category);
+                    printf("Item category changed successfully!\n");
+                    break;
+                case 5:
+                    printf("Enter new item description: ");
+                    scanf("%s", items[i].itemDescription);
+                    printf("Item description changed successfully!\n");
+                    break;
+                case 6:
+                    printf("Exiting edit menu...\n");
+                    break;
+                default:
+                    printf("Invalid choice!\n");
+                    break;
+                }
+            } while (editChoice != 6);
+        }
+    }
+
+    if (found == 0)
+    {
+        printf("Product ID not found!\n");
+    }
 }
